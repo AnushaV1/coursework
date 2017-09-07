53bc348f5f2c97cb78e0708f9e16ffa5121c96a7
Anusha <anush_chander@yahoo.com>
2017-09-06 22:27:32 -0700

    good

--- app.js
+++ app.js
@@ -34,13 +34,18 @@
  
 // })();
 
-
  (function () {
  'use strict';
 
 angular.module('LunchCheck', [])
  .controller('LunchCheckController', function ($scope) {
  $scope.menu = "";//get the list of items from the textbox
-  $scope.displayMessage = $scope.menu;
+ 
+ $scope.counter = function () {
+   // var totalNameValue = calculatNumericForString($scope.name);
+    //$scope.totalValue = totalNameValue;
+	$scope.displayMessage = $scope.menu;
+	};
+  
  });
  })();
