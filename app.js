fe299cb911ec6d9887a8a42d1329b55eccf77d73
Anusha <anush_chander@yahoo.com>
2017-09-06 22:40:59 -0700

    156

--- app.js
+++ app.js
@@ -4,11 +4,11 @@
 angular.module('LunchCheck', [])
  .controller('LunchCheckController', function ($scope) {
  //$scope.menu = "";//get the list of items from the textbox
- 
- $scope.counter = function () {
  if($scope.menu == "") {
    $scope.displayMessage = "Please enter data first"; 
     }
+ $scope.counter = function () {
+ 
    // var totalNameValue = calculatNumericForString($scope.name);
     //$scope.totalValue = totalNameValue;
 //	$scope.displayMessage = $scope.menu;
