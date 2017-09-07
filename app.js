91df29be536858c64ef962814a64da6297507cd4
Anusha <anush_chander@yahoo.com>
2017-09-06 22:20:51 -0700

    latest

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-(function () {
-'use strict';
+// (function () {
+// 'use strict';
+
+// angular.module('LunchCheck', [])
+// .controller('LunchCheckController', function ($scope) {
+
+// LunchCheckController.$inject = ['$scope'];
+  // $scope.menu = "";//get the list of items from the textbox
+  // $scope.totalValue = 0;
+  // console.log($scope.menu);
+   // if($scope.menu ==" ") {
+   // $scope.displayMessage = "Please enter data first"; 
+   // }
+   
+  // $scope.counter = function () {
+    // var msg = splitString($scope.menu,comma);
+   
+   // $scope.displayStatus = msg; 
+     
+   // function splitString(stringToSplit, separator) {
+  // var arrayOfStrings = stringToSplit.split(separator);
+  // if(arrayOfStrings.length <=3) {
+  // var message = "Enjoy!!";
+  // }
+  // else {
+  // var message = "Too much!";
+  // }
+  
+// }
+// return message;
+  // };
+  
+ // );
+ 
+// })();
+
+
+ (function () {
+ 'use strict';
 
 angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
-
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController ($scope) {
-  $scope.menu = "";//get the list of items from the textbox
-  $scope.totalValue = 0;
-  console.log($scope.menu);
-   if($scope.menu ==" ") {
-   $scope.displayMessage = "Please enter data first"; 
-   }
-   
-  $scope.counter = function () {
-    var msg = splitString($scope.menu,comma);
-   
-   $scope.displayStatus = msg; 
-     
-   function splitString(stringToSplit, separator) {
-  var arrayOfStrings = stringToSplit.split(separator);
-  if(arrayOfStrings.length <=3) {
-  var message = "Enjoy!!";
-  }
-  else {
-  var message = "Too much!";
-  }
-  
-}
-return message;
-  };
-  
- 
-
-})();
+ .controller('LunchCheckController', function ($scope) {
+ $scope.menu = "";//get the list of items from the textbox
+  $scope.displayMessage = $scope.menu;
+ });
+ })();
