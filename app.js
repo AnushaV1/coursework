4bb935b5bfbe467dfc05bfc9f24c199089b5f527
Anusha <anush_chander@yahoo.com>
2017-09-06 22:30:37 -0700

    124

--- app.js
+++ app.js
@@ -1,38 +1,3 @@
-// (function () {
-// 'use strict';
-
-// angular.module('LunchCheck', [])
-// .controller('LunchCheckController', function ($scope) {
-
-// LunchCheckController.$inject = ['$scope'];
-  // $scope.menu = "";//get the list of items from the textbox
-  // $scope.totalValue = 0;
-  // console.log($scope.menu);
-   // if($scope.menu ==" ") {
-   // $scope.displayMessage = "Please enter data first"; 
-   // }
-   
-  // $scope.counter = function () {
-    // var msg = splitString($scope.menu,comma);
-   
-   // $scope.displayStatus = msg; 
-     
-   // function splitString(stringToSplit, separator) {
-  // var arrayOfStrings = stringToSplit.split(separator);
-  // if(arrayOfStrings.length <=3) {
-  // var message = "Enjoy!!";
-  // }
-  // else {
-  // var message = "Too much!";
-  // }
-  
-// }
-// return message;
-  // };
-  
- // );
- 
-// })();
 (function () {
  'use strict';
 
