4a89d06bd42ca640440fca196af0727793caffc2
Anusha <anush_chander@yahoo.com>
2017-09-06 23:16:03 -0700

    456

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ angular.module('LunchCheck', [])
  LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
      $scope.counter = function () {
- // console.log($scope.menu);
+  console.log($scope.menu);
    // var totalNameValue = calculatNumericForString($scope.name);
     //$scope.totalValue = totalNameValue;
 	$scope.displayMessage = $scope.menu;
